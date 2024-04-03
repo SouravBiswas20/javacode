4a191b7951f3377256a7e8c815e7a20f2c914112
SouravBiswas20 <biswassourav121@gmail.com>
2024-04-04 00:08:45 +0530

    first commit

---
+++ src/week1/Datatypedemo.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Datatypedemo 
+{
+
+	
+	
+	
+	
+}
---
+++ src/week1/Firstcode.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Firstcode {
+	
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("hello all!");
+		
+				
+	}
+	
+	
+
+}
